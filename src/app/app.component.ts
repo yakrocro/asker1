a568b9619e9af41f73cd7d96a06021ea45185004
Junior <kouasjunior@gmail.com>
2019-08-26 10:43:46 -0400

    ident-form validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'asker1';
+
+  // userModel = new User('juju', 'juju@email.com', 'mdp');
+
 }
--- src/app/ident-form/ident-form.component.ts
+++ src/app/ident-form/ident-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-ident-form',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ident-form.component.css']
 })
 export class IdentFormComponent implements OnInit {
+
+  userModel = new User('juju', 'juju@email.com', 'mdp');
 
   constructor() { }
 
